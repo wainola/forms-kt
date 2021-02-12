efd170714ae87dcbeaba09320fe904ff46c73af1
Nicolas Riquelme <nrriquelme@gmail.com>
2021-02-12 01:29:21 -0300

    quick fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className="flex flex-col h-screen justify-center">
-      <div className="flex flex-col justify-center border-2 ml-16 mr-16 border-red-300 shadow h-3/6 bg-white">
+      <div className="flex flex-col justify-center border-2 ml-16 mr-16 border-red-300 shadow h-3/6 bg-white rounded-md">
         <div className="flex flex-col justify-center h-full">
           <div className="flex flex-row justify-center mt-5 mb-5">
             <h2 className="text-7xl">Hive</h2>
@@ -13,12 +13,12 @@ export default function Home() {
         </div>
         <div className="flex flex-row justify-center h-3/6">
           <div className="flex flex-row justify-between items-center h-3/6 w-3/6">
-            <Link href="/reimbursment">
+            <Link href="/register">
               <button className="bg-red-400 rounded-md p-4 shadow-sm text-white font-semibold">
                 Go to Register
               </button>
             </Link>
-            <Link href="/register">
+            <Link href="/reimbursment">
               <button className="bg-red-400 rounded-md p-4 shadow-sm text-white font-semibold">
                 Go to reimbursement
               </button>
